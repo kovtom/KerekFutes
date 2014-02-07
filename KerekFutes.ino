2ea0073534214d74906b79777f4bb5112cbbe567
Tamas Kovacs <reg@kovnet.hu>
2014-02-07 20:58:09 +0100

    Button_lib_proba OK

--- KerekFutes.ino
+++ KerekFutes.ino
@@ -19,8 +19,9 @@
 // Declare which fonts we will be using
 //extern uint8_t SmallFont[];
 extern uint8_t BigFont[];
-extern uint8_t DotMatrix_M[];
+extern uint8_t Dingbats1_XL[];
 extern uint8_t SmallFont[];
+extern uint8_t DotMatrix_M[];
 //extern uint8_t Ubuntu[];
 
 // ILI9325C kijelzo bekotese:
@@ -47,11 +48,18 @@ ILI9325C_tft  myGLCD(ILI9325C, PC_6, PC_5, PC_7, PC_4);
 //UTouch(byte tclk, byte tcs, byte tdin, byte dout, byte irq);
 UTouch myTouch(PA_6, PA_5, PA_4, PA_3, PA_2);
 
+// Finally we set up UTFT_Buttons :)
+UTFT_Buttons  myButtons(&myGLCD, &myTouch);
+
 
 int x, y;
 char stCurrent[20]="";
 int stCurrentLen=0;
 char stLast[20]="";
+
+//
+//Welcome screen varakozasai ido msec
+const unsigned int WELCOME_SRC_TIMEOUT = 1;
 
 
 /*************************
@@ -69,9 +77,9 @@ void setup()
 	
 	welcomeScreen();
 
-	myGLCD.setFont(BigFont);
-	myGLCD.setBackColor(0, 0, 255);
-	drawButtons();  
+	myGLCD.setFont(SmallFont);
+	myGLCD.clrScr();
+	myButtons.setTextFont(BigFont);
 }
 
 /*************************
@@ -97,195 +105,45 @@ void welcomeScreen()
 		myGLCD.drawLine(i,202,i,208);
 		delay(10);
 	}
-	delay(2500);
+	delay(WELCOME_SRC_TIMEOUT);
 	myGLCD.clrScr();
 
 	tone(TONE_PIN, 1000, 50);
 }
 
-void drawButtons()
-{
-	// Draw the upper row of buttons
-	for (x=0; x<5; x++)
-	{
-		myGLCD.setColor(0, 0, 255);
-		myGLCD.fillRoundRect (10+(x*60), 10, 60+(x*60), 60);
-		myGLCD.setColor(255, 255, 255);
-		myGLCD.drawRoundRect (10+(x*60), 10, 60+(x*60), 60);
-		myGLCD.printNumI(x+1, 27+(x*60), 27);
-	}
-	// Draw the center row of buttons
-	for (x=0; x<5; x++)
-	{
-		myGLCD.setColor(0, 0, 255);
-		myGLCD.fillRoundRect (10+(x*60), 70, 60+(x*60), 120);
-		myGLCD.setColor(255, 255, 255);
-		myGLCD.drawRoundRect (10+(x*60), 70, 60+(x*60), 120);
-		if (x<4)
-			myGLCD.printNumI(x+6, 27+(x*60), 87);
-	}
-	myGLCD.print("0", 267, 87);
-	// Draw the lower row of buttons
-	myGLCD.setColor(0, 0, 255);
-	myGLCD.fillRoundRect (10, 130, 150, 180);
-	myGLCD.setColor(255, 255, 255);
-	myGLCD.drawRoundRect (10, 130, 150, 180);
-	myGLCD.print("Clear", 40, 147);
-	myGLCD.setColor(0, 0, 255);
-	myGLCD.fillRoundRect (160, 130, 300, 180);
-	myGLCD.setColor(255, 255, 255);
-	myGLCD.drawRoundRect (160, 130, 300, 180);
-	myGLCD.print("Enter", 190, 147);
-	myGLCD.setBackColor (0, 0, 0);
-}
-
-void updateStr(int val)
-{
-	if (stCurrentLen<20)
-	{
-		stCurrent[stCurrentLen]=val;
-		stCurrent[stCurrentLen+1]='\0';
-		stCurrentLen++;
-		myGLCD.setColor(0, 255, 0);
-		myGLCD.print(stCurrent, LEFT, 224);
-	}
-	else
-	{
-		myGLCD.setColor(255, 0, 0);
-		myGLCD.print("BUFFER FULL!", CENTER, 192);
-		delay(500);
-		myGLCD.print("            ", CENTER, 192);
-		delay(500);
-		myGLCD.print("BUFFER FULL!", CENTER, 192);
-		delay(500);
-		myGLCD.print("            ", CENTER, 192);
-		myGLCD.setColor(0, 255, 0);
-	}
-}
-
-// Draw a red frame while a button is touched
-void waitForIt(int x1, int y1, int x2, int y2)
-{
-	myGLCD.setColor(255, 0, 0);
-	myGLCD.drawRoundRect (x1, y1, x2, y2);
-	while (myTouch.dataAvailable())
-		myTouch.read();
-	tone(TONE_PIN, 1047, 30);
-	tone(TONE_PIN, 2093, 30);
-	tone(TONE_PIN, 4186, 30);
-
-	myGLCD.setColor(255, 255, 255);
-	myGLCD.drawRoundRect (x1, y1, x2, y2);
-}
-
 
 void loop()
 {
-	while (true)
-	{
-		if (myTouch.dataAvailable())
+	int but1, pressed_button;
+	boolean default_colors = true;
+
+	but1 = myButtons.addButton( 10,  20, 300,  30, "Button 1");
+
+	unsigned long prevTime = 0;
+
+	myButtons.drawButtons();
+
+	myGLCD.print("You pressed:", 110, 205);
+	myGLCD.setColor(VGA_BLACK);
+	myGLCD.setBackColor(VGA_WHITE);
+	myGLCD.print("None", 110, 220);
+
+	while(1) 
+	{	
+		if (myTouch.dataAvailable() == true && millis() > prevTime)
 		{
-			myTouch.read();
-			x=myTouch.getX();
-			y=myTouch.getY();
+			prevTime = millis() + 100;
+			pressed_button = myButtons.checkButtons();
 
-			if ((y>=10) && (y<=60))  // Upper row
+			if (pressed_button==but1)
 			{
-				if ((x>=10) && (x<=60))  // Button: 1
-				{
-					waitForIt(10, 10, 60, 60);
-					updateStr('1');
-				}
-				if ((x>=70) && (x<=120))  // Button: 2
-				{
-					waitForIt(70, 10, 120, 60);
-					updateStr('2');
-				}
-				if ((x>=130) && (x<=180))  // Button: 3
-				{
-					waitForIt(130, 10, 180, 60);
-					updateStr('3');
-				}
-				if ((x>=190) && (x<=240))  // Button: 4
-				{
-					waitForIt(190, 10, 240, 60);
-					updateStr('4');
-				}
-				if ((x>=250) && (x<=300))  // Button: 5
-				{
-					waitForIt(250, 10, 300, 60);
-					updateStr('5');
-				}
+				tone(TONE_PIN, 2500, 100);
+
+				myGLCD.print("Button 1", 110, 220);
 			}
-
-			if ((y>=70) && (y<=120))  // Center row
+			else
 			{
-				if ((x>=10) && (x<=60))  // Button: 6
-				{
-					waitForIt(10, 70, 60, 120);
-					updateStr('6');
-				}
-				if ((x>=70) && (x<=120))  // Button: 7
-				{
-					waitForIt(70, 70, 120, 120);
-					updateStr('7');
-				}
-				if ((x>=130) && (x<=180))  // Button: 8
-				{
-					waitForIt(130, 70, 180, 120);
-					updateStr('8');
-				}
-				if ((x>=190) && (x<=240))  // Button: 9
-				{
-					waitForIt(190, 70, 240, 120);
-					updateStr('9');
-				}
-				if ((x>=250) && (x<=300))  // Button: 0
-				{
-					waitForIt(250, 70, 300, 120);
-					updateStr('0');
-				}
-			}
-
-			if ((y>=130) && (y<=180))  // Upper row
-			{
-				if ((x>=10) && (x<=150))  // Button: Clear
-				{
-					waitForIt(10, 130, 150, 180);
-					stCurrent[0]='\0';
-					stCurrentLen=0;
-					myGLCD.setColor(0, 0, 0);
-					myGLCD.fillRect(0, 224, 319, 239);
-				}
-				if ((x>=160) && (x<=300))  // Button: Enter
-				{
-					waitForIt(160, 130, 300, 180);
-					if (stCurrentLen>0)
-					{
-						for (x=0; x<stCurrentLen+1; x++)
-						{
-							stLast[x]=stCurrent[x];
-						}
-						stCurrent[0]='\0';
-						stCurrentLen=0;
-						myGLCD.setColor(0, 0, 0);
-						myGLCD.fillRect(0, 208, 319, 239);
-						myGLCD.setColor(0, 255, 0);
-						myGLCD.print(stLast, LEFT, 208);
-					}
-					else
-					{
-						myGLCD.setColor(255, 0, 0);
-						myGLCD.print("BUFFER EMPTY", CENTER, 192);
-						delay(500);
-						myGLCD.print("            ", CENTER, 192);
-						delay(500);
-						myGLCD.print("BUFFER EMPTY", CENTER, 192);
-						delay(500);
-						myGLCD.print("            ", CENTER, 192);
-						myGLCD.setColor(0, 255, 0);
-					}
-				}
+				myGLCD.print("None    ", 110, 220);
 			}
 		}
 	}
